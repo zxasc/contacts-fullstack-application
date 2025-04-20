89fa85ae00c60b9c68db8f8f7bf9d201907d83f0
pawel <pawel@lechocki.com>
2025-04-20 21:39:16 +0200

    add tailwind styles, translate ui to polish (mostly)

--- backend/configuration_root/urls.py
+++ backend/configuration_root/urls.py
@@ -1,18 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from project_root.contacts_api import views as contact_views
 from project_root.weather_grabber import views as weather_views
 
-router = DefaultRouter()
-router.register(r'api/contacts', contact_views.ContactViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Weather path, grabs city param from the url
     path('weather/<str:city>', weather_views.get_weather_data),
     # Contacts API paths
-    # path('', include(router.urls)),
     path('api/contacts/', contact_views.ContactViewSet.as_view({
         'get': 'list',
         'post': 'create'
